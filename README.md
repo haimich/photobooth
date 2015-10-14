a57ebee4f9e06d847c761410f930328e8b6f718e
Michael Müller <haimich@gmx.net>
2015-10-14 23:22:01 +0200

    added working demo

--- README.md
+++ README.md
@@ -4,28 +4,16 @@ A JavaScript photobooth application where you can pick an own background image.
 ## Libraries
 
 https://gitlab.com/multimedia/chromakey-js
-=> removes green from static images in a canvas
+=> removes green from static images in a canvas; many tweak options
 
 http://codepen.io/teetteet/pen/tKqEs
-=> Chroma direkt auf Video
+=> Chroma direkt auf Video (klappt nicht gut mit Tuch)
 
 https://github.com/felipenmoura/js-chroma-key
-=> canvas + video
-
-### to be tested
+=> canvas + video (klappt nicht gut mit Tuch)
 
 https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Manipulating_video_using_canvas
-http://photobooth.orange-coding.net/
-
-### Nette Photobooth
-* https://webcamtoy.com/de/
-* speichern im Downloads-Ordner
-
-### Sound effect
-* https://github.com/jhuckaby/webcamjs
-
-### Face Detection
-* http://auduno.github.io/headtrackr/examples/facetracking.html
+=> Demo nicht mit Live Video; gut erklärt; der Code schneidet einfach grüne Pixel raus
 
 ### Merge images
 * http://stackoverflow.com/questions/6765370/merge-image-using-javascript
@@ -34,4 +22,17 @@ http://photobooth.orange-coding.net/
 * https://www.youtube.com/watch?v=Y1XaBJZ8Xcg
 
 ### Upload to PHP server
-* https://www.twilio.com/blog/2015/03/create-a-browser-based-photobooth-with-javascript-php-and-twilio.html
+
+https://www.twilio.com/blog/2015/03/create-a-browser-based-photobooth-with-javascript-php-and-twilio.html
+=> Php + Twilio
+
+### Addons
+
+https://webcamtoy.com/de/
+=> nette Effekt, speichert im Downloads-Ordner
+
+https://github.com/jhuckaby/webcamjs
+=> Shutter sound effect
+
+http://auduno.github.io/headtrackr/examples/facetracking.html
+=> Face Detection
--- webcam-chroma/getVideo.js
+++ webcam-chroma/getVideo.js
@@ -1,26 +1,61 @@
 (function () {
   'use strict';
-  
+
   var video = document.querySelector('#videoElement');
   var canvas = document.querySelector('#canvasElement');
-  var img = document.querySelector('#imgElement');
-
   var ctx = canvas.getContext('2d');
+
   var localMediaStream = null;
 
   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
 
+  function buildConfig() {
+    var sliderHue = document.querySelector('input[name=hue]'),
+        sliderDelta1 = document.querySelector('input[name=delta1]'),
+        sliderDelta2 = document.querySelector('input[name=delta2]'),
+        sliderDelta3 = document.querySelector('input[name=delta3]'),
+        sliderMaxLight = document.querySelector('input[name=maxlight]'),
+        sliderMinLight = document.querySelector('input[name=minlight]'),
+        sliderMinSaturation = document.querySelector('input[name=minSaturation]');
+
+    return {
+      input: canvas,
+      hueKey: sliderHue.value,
+      hueDelta1: sliderDelta1.value,
+      hueDelta2: sliderDelta2.value,
+      hueDelta3: sliderDelta3.value,
+      maxLight: sliderMaxLight.value / 100,
+      minLight: sliderMinLight.value / 100,
+      minSatur: sliderMinSaturation.value / 100
+    };
+  }
+
   function snapshot() {
     if (localMediaStream) {
-      ctx.drawImage(video, 0, 0, img.width, img.height,        // source rectangle
-                           0, 0, canvas.width, canvas.height); // destination rectangle
+      ctx.drawImage(video, 0, 0, 300, 200);
       
-      // "image/webp" works in Chrome. Other browsers will fall back to image/png.
-      img.src = canvas.toDataURL('image/webp');
+      var alpha = new window.ChromaKey2Alpha(buildConfig());
+      alpha.removeChromaKey();
     }
   }
 
+  function timerCallback() {
+    if (video.paused || video.ended) {
+      return;
+    }
+
+    snapshot();
+    setTimeout(function () {
+      timerCallback();
+    }, 0);
+  }
+
   video.addEventListener('click', snapshot, false);
+  video.addEventListener('play', function() {
+    // var width = video.videoWidth / 2;
+    // var height = video.videoHeight / 2;
+    timerCallback();
+  }, false);
 
   function handleVideo(stream) {
     video.src = window.URL.createObjectURL(stream);

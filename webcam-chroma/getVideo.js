78d2bcb311f3e1c29539cd2b23ccc6e6e0485d53
Michael Müller <haimich@gmx.net>
2015-10-18 11:10:34 +0200

    johannes fix

--- webcam-chroma/getVideo.js
+++ webcam-chroma/getVideo.js
@@ -7,9 +7,12 @@
     var bgImg = document.querySelector('#bg-img');
 
     var chromaCanvas = document.querySelector('#chromaCanvas');
-    var mergeCanvas = document.querySelector('#mergeCanvas');
+    var mergeCanvas = document.createElement('canvas');
+    mergeCanvas.width = chromaCanvas.width;
+    mergeCanvas.height = chromaCanvas.height;
     var chromaCtx = chromaCanvas.getContext('2d');
     var mergeCtx = mergeCanvas.getContext('2d');
+    var snapshotElement = document.querySelector('#snapshot');
 
     var localMediaStream = null;
 
@@ -25,7 +28,7 @@
           sliderMinSaturation = document.querySelector('input[name=minSaturation]');
 
       return {
-        input: chromaCanvas,
+        input: mergeCanvas,
         hueKey: sliderHue.value,
         hueDelta1: sliderDelta1.value,
         hueDelta2: sliderDelta2.value,
@@ -38,10 +41,12 @@
 
     function fillChromaCanvas() {
       if (localMediaStream) {
-        chromaCtx.drawImage(video, 0, 0, 300, 200);
+        mergeCtx.drawImage(video, 0, 0, video.clientWidth,    video.clientHeight,
+                   0, 0, chromaCanvas.width, chromaCanvas.height);
         
         var chromaKeyer = new window.ChromaKey2Alpha(buildConfig());
         chromaKeyer.removeChromaKey();
+        chromaCtx.drawImage(mergeCanvas, 0, 0);
       }
     }
 
@@ -50,27 +55,47 @@
         return;
       }
 
+
+      var offsetY = (bgImg.naturalWidth / 4 * 3 - bgImg.naturalHeight) / 2;
+
+//chromaCtx.clearRect(0, 0, chromaCanvas.width, chromaCanvas.height);
+chromaCtx.drawImage(bgImg, 0, 0, bgImg.naturalWidth,    bgImg.naturalHeight,
+                   0, 0, chromaCanvas.width, chromaCanvas.height);
+
       fillChromaCanvas();
       setTimeout(function () {
         timerCallback();
       }, 0);
     }
 
-    function snapshot() {
-      //1. Add background image
-      mergeCtx.drawImage(bgImg, 0, 0, bgImg.naturalWidth, bgImg.naturalHeight);
-
-      //2. Add current video image with chroma key applied
-
-      mergeCtx.drawImage(chromaCanvas, 0, 0);
+    function loadImage(sourceURL) {
+      return new Promise(function (resolve, reject) {
+        var imageElement = document.createElement('img');
+        imageElement.crossorigin = 'anonymous';
+        imageElement.src = sourceURL;
+        imageElement.onload = function () { resolve(imageElement); };
+        imageElement.onerror = reject;
+      });
     }
 
-    video.addEventListener('click', snapshot, false);
+loadImage('https://farm1.staticflickr.com/592/22266513761_3d92f74958_h.jpg').then(function (img) { bgImg = img; console.log(img); });
+
+    function snapshot() {
+      //1. Add background image
+      //mergeCtx.drawImage(bgImg, 0, 0, bgImg.naturalWidth, bgImg.naturalHeight);
+
+      //2. Add current video image with chroma key applied
+      //mergeCtx.drawImage(chromaCanvas, 0, 0);
+
+      snapshotElement.src = chromaCanvas.toDataURL();
+    }
+
+    chromaCanvas.addEventListener('click', snapshot);
     video.addEventListener('play', function() {
       // var width = video.videoWidth / 2;
       // var height = video.videoHeight / 2;
       timerCallback();
-    }, false);
+    });
 
     function handleVideo(stream) {
       video.src = window.URL.createObjectURL(stream);

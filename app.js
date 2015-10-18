add07074dc119f941878ecde23da502a6751359f
Michael Müller <haimich@gmx.net>
2015-10-18 12:18:41 +0200

    basic flickr search working

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-'use strict';
-
-var yaml    = require('js-yaml');
-var fs      = require('fs');
+var search  = require('./search');
+var config  = require('./config');
 var express = require('express');
-
-function readConfig() {
-  try {
-    return yaml.safeLoad(fs.readFileSync('photobooth-config.yml', 'utf8'));
-  } catch (e) {
-    throw new Error('Config could not be loaded');
-  }
-}
-
-var config = readConfig();
 
 var app = express();
 app.use(express.static('static'));
 
 app.get('/search', function (req, res) {
-  res.send('Hello World');
+  var term = req.query.term || '';
+
+  if (term === '') {
+    res.end('Error: no term given');
+  } else {
+    try {
+      search(term, function(response) {
+        res.end(response);
+      });
+    } catch (error) {
+      res.end(error);
+    }
+  }
 });
  
 app.listen(3000);
